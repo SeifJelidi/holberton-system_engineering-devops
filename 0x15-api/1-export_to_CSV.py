5a6f8a0f2f5981babbe6c155381c330707cedf77
Seif Jelidi <seifjelidi3@gmail.com>
2020-09-14 22:27:02 +0000

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,13 +17,20 @@ if __name__ == "__main__":
 
     emp_name = user_json.get('name')
 
+    task_list = []
+    for task in tasks_json:
+        if int(id) == task.get('userId'):
+            row = {}
+            row["USER_ID"] = id
+            row["USERNAME"] = emp_name
+            row["TASK_COMPLETED_STATUS"] = task.get('completed')
+            row["TASK_TITLE"] = task.get('title')
+            task_list.append(row)
+
     filename = "{}.csv".format(id)
     fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
     with open(filename, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-        for task in tasks_json:
-            if int(id) == task.get('userId'):
-                csv_writer.writerow([int(id), emp_name, task.get('completed'),
-                                    task.get('title')])
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames,
+                                    quoting=csv.QUOTE_ALL)
+        csv_writer.writerows(task_list)
