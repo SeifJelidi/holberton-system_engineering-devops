b6511a66c4a3b2649bd9acb8a6d9ec3a6dd0c58d
SeifJelidi <seifjelidi3@gmail.com>
2020-10-08 21:11:54 +0000

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,12 @@
 #fix in nginx
 exec { 'fixing':
 echo "fixing"
-command => 'sed -i "s/15/2000/" /etc/default/nginx',
-  path    => ['/bin/']
+command => 'sed -i' "s/15/2000/" '/etc/default/nginx',
+path    => ['/bin/']
 }
 
-exec { 'restart_nginx':
+exec { 'restart_server':
 echo "restart"
 command  => 'service nginx restart',
 }
+
